5d6c91821c5caa6cdcf5b341497b3f2ce55ffa49
shaifsab <iamshaifahmed@gmail.com>
2024-08-23 06:06:19 +0600

    last

--- src/firebase.config.js
+++ src/firebase.config.js
@@ -7,13 +7,13 @@ import { getDatabase } from "firebase/database";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCCsctIwE7qjC40TEMeAbk38UybL9wYIkE",
-  authDomain: "chatting-app-c349f.firebaseapp.com",
-  projectId: "chatting-app-c349f",
-  storageBucket: "chatting-app-c349f.appspot.com",
-  messagingSenderId: "941832530697",
-  appId: "1:941832530697:web:396b2bf995362ebab801d8",
-  measurementId: "G-9XXG8BVRCE"
+apiKey: "AIzaSyDFBNAZw12T3klVokfpeSLTb73pza6BOTo",
+authDomain: "easychatting-f5d09.firebaseapp.com",
+projectId: "easychatting-f5d09",
+storageBucket: "easychatting-f5d09.appspot.com",
+messagingSenderId: "238981480717",
+appId: "1:238981480717:web:ff8798b2bac8f24ef6f044",
+measurementId: "G-TMEN6HWQLX"
 };
 
 // Initialize Firebase
